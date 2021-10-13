5c1d43dfece34db02eb8d2ae52bdf1ddbfe2012e
Dan Knight <danknight@mednet.ucla.edu>
2021-10-13 12:38:07 -0700

    Test all samples

--- tests/testthat/test-msk.R
+++ tests/testthat/test-msk.R
@@ -1,15 +1,17 @@
 test_that(
     'Non-Branching Examples', {
-        sample <- 'WHO003';
-        sampled <- sampled <- test.sample(sample, trees_df, cnas_df, snvs_df);
-        test.tree(
-            tree = sampled$tree,
-            cnas = sampled$cnas,
-            snvs = sampled$snvs,
-            sample = sample,
-            add_polygons = TRUE,
-            add_normal = TRUE,
-            add_genes = TRUE
-            );
+        for (sample in unique(trees_df$Sample)) {
+            sampled <- sampled <- test.sample(sample, trees_df, cnas_df, snvs_df);
+            
+            test.tree(
+                tree = sampled$tree,
+                cnas = sampled$cnas,
+                snvs = sampled$snvs,
+                sample = sample,
+                add_polygons = TRUE,
+                add_normal = TRUE,
+                add_genes = TRUE
+                );
+            }
         }
     );
