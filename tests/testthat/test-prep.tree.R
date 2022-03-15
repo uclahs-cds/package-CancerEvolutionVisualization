8c35e330a16c47e2a0242fbc719518aff9788469
Dan Knight <danknight@mednet.ucla.edu>
2022-03-15 10:45:40 -0700

    Update and expand tests with CP column changes

--- tests/testthat/test-prep.tree.R
+++ tests/testthat/test-prep.tree.R
@@ -1,10 +1,20 @@
 test_that(
     'prep.tree handles invalid parent column', {
-    invalid.tree <- data.frame();
+    invalid.parent.tree <- data.frame();
     
     expect_error(
-        prep.tree(tree.df = invalid.tree),
+        prep.tree(tree.df = invalid.parent.tree),
         regexp = 'parent'
+        );
+    });
+
+test_that(
+    'prep.tree handles invalid CP column', {
+    invalid.CP.tree <- data.frame(parent = c(NA));
+    
+    expect_error(
+        prep.tree(tree.df = invalid.CP.tree),
+        regexp = 'CP'
         );
     });
 
@@ -25,5 +35,16 @@ test_that(
         expect_equal(
             CancerEvolutionVisualization:::prep.tree.parent(parent)[1],
             -1
+            );
+    });
+
+test_that(
+    'prep.tree.parent has correct output length', {
+        parent <- c(0, NA, 1, 3, 2);
+        expected.length <- length(parent);
+        
+        expect_length(
+            CancerEvolutionVisualization:::prep.tree.parent(parent),
+            expected.length
             );
     });
