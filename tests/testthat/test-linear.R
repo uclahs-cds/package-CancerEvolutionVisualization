dd3f909b4f5e444e8a15eef3d611948a67cc48cd
Dan Knight <danknight@mednet.ucla.edu>
2022-03-17 16:46:21 -0700

    Reorganise test data

--- tests/testthat/test-linear.R
+++ tests/testthat/test-linear.R
@@ -1,13 +1,14 @@
 test_that(
     'Linear case values', {
-        load('data/linear.Rda');
+        load('data/linear.plots.Rda');
+        load('data/linear.data.Rda')
 
         expect_true(compare.trees(
             linear.example,
             create.test.tree(
-                trees_df,
-                cnas_df,
-                snvs_df,
+                linear.test.data$tree,
+                linear.test.data$cnas,
+                linear.test.data$snvs,
                 sample.name,
                 add_normal = TRUE,
                 add_genes = TRUE,
--- tests/testthat/test-options.R
+++ tests/testthat/test-options.R
@@ -1,36 +1,38 @@
 test_that(
 'Linear Example', {
-        expect_true(!is.null(
-            create.test.tree(
-                tree = trees_df,
-                cnas = cnas_df,
-                snvs = snvs_df,
-                sample = sample,
-                add_normal = TRUE,
-                add_genes = TRUE,
-                yaxis_position = 'both'
-                )
-            ));
+    load('data/linear.data.Rda');
+    
+    expect_true(!is.null(
+        create.test.tree(
+            tree = linear.test.data$tree,
+            cnas = linear.test.data$cnas,
+            snvs = linear.test.data$snvs,
+            sample = sample,
+            add_normal = TRUE,
+            add_genes = TRUE,
+            yaxis_position = 'both'
+            )
+        ));
 
-        expect_true(!is.null(
-            create.test.tree(
-                tree = trees_df,
-                cnas = cnas_df,
-                snvs = snvs_df,
-                sample = sample,
-                label_nodes = FALSE,
-                add_polygons = FALSE,
-                yaxis_position = 'both'
-                )
-            ));
+    expect_true(!is.null(
+        create.test.tree(
+            tree = linear.test.data$tree,
+            cnas = linear.test.data$cnas,
+            snvs = linear.test.data$snvs,
+            sample = sample,
+            label_nodes = FALSE,
+            add_polygons = FALSE,
+            yaxis_position = 'both'
+            )
+        ));
 
-        expect_true(!is.null(
-            create.test.tree(
-                tree = trees_df,
-                cnas = cnas_df,
-                snvs = snvs_df,
-                sample = sample,
-                yaxis_position = 'left'
-                )
-            ));
-        });
+    expect_true(!is.null(
+        create.test.tree(
+            tree = linear.test.data$tree,
+            cnas = linear.test.data$cnas,
+            snvs = linear.test.data$snvs,
+            sample = sample,
+            yaxis_position = 'left'
+            )
+        ));
+    });
