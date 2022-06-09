3b4bd33e1e010a538a5ca36be1d95a627c830047
Dan Knight <danknight@mednet.ucla.edu>
2022-06-09 11:40:13 -0700

    Fix linting errors in tests

--- tests/testthat/helper-multitest.R
+++ tests/testthat/helper-multitest.R
@@ -11,24 +11,24 @@ create.test.tree <- function(tree, genes, sample, ...) {
         gene.cex = 0.85,
         seg1.col = 'navy',
         seg2.col = 'gold',
-        node.col = "grey40",
+        node.col = 'grey40',
         sig.curve = 3,
         line.lwd = 4,
         xaxis.space.left = 0.1,
         xaxis.space.right = 0.1,
         yaxis1.interval = 10,
         min.width = 1,
-        yaxis1.label = "PGA",
-        yaxis2.label = "SNV",
+        yaxis1.label = 'PGA',
+        yaxis2.label = 'SNV',
         yaxis2.interval = 1000,
         spread = 1.1,
-        xaxis.label = "CP",
+        xaxis.label = 'CP',
         title = sample,
         title.cex = 1.55,
         title.y = 0.3,
-        title.y.units = "inches",
+        title.y.units = 'inches',
         ...
         );
-    
+
     return(out[[1]]);
     }
--- tests/testthat/test-options.R
+++ tests/testthat/test-options.R
@@ -1,36 +1,36 @@
 test_that(
-'Linear Example', {
-    load('data/linear.data.Rda');
-    sample <- 'WHO003';
-    
-    expect_true(!is.null(
-        create.test.tree(
-            linear.test.data$tree,
-            linear.test.data$genes,
-            sample = sample,
-            add.normal = TRUE,
-            add.genes = TRUE,
-            yaxis.position = 'both'
-            )
-        ));
+    'Linear Example', {
+        load('data/linear.data.Rda');
+        sample <- 'WHO003';
 
-    expect_true(!is.null(
-        create.test.tree(
-            linear.test.data$tree,
-            linear.test.data$genes,
-            sample = sample,
-            label.nodes = FALSE,
-            add.polygons = FALSE,
-            yaxis.position = 'both'
-            )
-        ));
+        expect_true(!is.null(
+            create.test.tree(
+                linear.test.data$tree,
+                linear.test.data$genes,
+                sample = sample,
+                add.normal = TRUE,
+                add.genes = TRUE,
+                yaxis.position = 'both'
+                )
+            ));
 
-    expect_true(!is.null(
-        create.test.tree(
-            linear.test.data$tree,
-            linear.test.data$genes,
-            sample = sample,
-            yaxis.position = 'left'
-            )
-        ));
+        expect_true(!is.null(
+            create.test.tree(
+                linear.test.data$tree,
+                linear.test.data$genes,
+                sample = sample,
+                label.nodes = FALSE,
+                add.polygons = FALSE,
+                yaxis.position = 'both'
+                )
+            ));
+
+        expect_true(!is.null(
+            create.test.tree(
+                linear.test.data$tree,
+                linear.test.data$genes,
+                sample = sample,
+                yaxis.position = 'left'
+                )
+            ));
     });
