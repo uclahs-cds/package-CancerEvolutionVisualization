44bed5ff13302ce0de72d77ddc1ceb990bec1eee
Dan Knight <danknight@mednet.ucla.edu>
2022-06-28 23:31:42 -0700

    Add unit tests for yat prep

--- tests/testthat/test-axis.ticks.R
+++ tests/testthat/test-axis.ticks.R
@@ -1,3 +1,23 @@
+test_that('prep.yat handles valid yat', {
+    original.yat <- list(1:10, seq(0, 20));
+    result.yat <- prep.yat(original.yat);
+
+    expect_equivalent(result.yat, original.yat);
+    });
+
+test_that('prep.yat handles NULL input', {
+    result.yat <- prep.yat(NULL);
+    expected.result <- list();
+    
+    expect_equivalent(result.yat, expected.result);
+    });
+
+test_that('prep.yat warns on invalid input', {
+    invalid.yat <- 100;
+
+    expect_warning(prep.yat(invalid.yat));
+    });
+
 test_that(
     'get.default.yat values begin at 0', {
         result.yat <- get.default.yat(5, 2);
