41e1d2309cee1e0c0d9ac64c4f076e59dcff58e0
Dan Knight <danknight@mednet.ucla.edu>
2022-07-29 18:31:00 -0700

    Update tests and test plots

--- tests/testthat/helper-multitest.R
+++ tests/testthat/helper-multitest.R
@@ -2,7 +2,6 @@ create.test.tree <- function(tree, genes, sample, ...) {
     out <- SRCGrob(
         tree,
         genes,
-        wid = 2,
         node.radius = 0.1,
         gene.cex = 0.85,
         scale1 = 0.9,
@@ -12,7 +11,6 @@ create.test.tree <- function(tree, genes, sample, ...) {
         line.lwd = 4,
         yaxis1.label = 'PGA',
         yaxis2.label = 'SNV',
-        spread = 1.1,
         xaxis.label = 'CP',
         main = sample,
         main.cex = 1.55,
