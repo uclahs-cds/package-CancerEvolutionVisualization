634cb28c91b24294f8f70187d520cf1585dde9d3
Dan Knight <danknight@mednet.ucla.edu>
2023-02-24 18:22:42 -0800

    Define report inputs

--- R/CEV.report.R
+++ R/CEV.report.R
@@ -1,9 +1,18 @@
 CEV.report <- function(
+    phylogeny,
+    SNV.assignment,
+    SNV.counts,
+    CCF.values,
     output.filename,
     title,
     author,
     date = NULL
     ) {
+    phylogeny <- prep.phylogeny(phylogeny);
+    SNV.assignment <- prep.SNV.assignment(SNV.assignment);
+    SNV.counts <- prep.SNV.counts(SNV.counts);
+    CCF.values <- prep.CCF.values(CCF.values);
+    
     report.params <- list(
         title = title,
         author = author,
---
+++ R/prep.report.R
@@ -0,0 +1,15 @@
+prep.phylogeny <- function(phylogeny) {
+    return(phylogeny);
+    }
+
+prep.SNV.assignment <- function(SNV.assignment) {
+    return(SNV.assignment);
+    };
+
+prep.SNV.counts <- function(SNV.counts) {
+    return(SNV.counts);
+    };
+
+prep.CCF.values <- function(CCF.values) {
+    return(CCF.values);
+    };
