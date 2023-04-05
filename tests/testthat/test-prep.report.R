31e820a8f904ff5eddee499ef97c6b7dcaef27e2
Dan Knight <danknight@mednet.ucla.edu>
2023-04-05 04:00:13 -0700

    Column validation unit tests

--- tests/testthat/test-prep.report.R
+++ tests/testthat/test-prep.report.R
@@ -88,3 +88,30 @@ test_that('prep.CCF.values errors on missing CCF column', {
         regexp = 'CCF'
         );
     });
+
+test_that('validate.clone.ids handles valid clone IDs', {
+    input <- data.frame(clone.ids = c('A', 'B', 'C'));
+    expect_silent(validate.clone.ids(input, input, input));
+    });
+
+test_that('validate.clone.ids handles invalid SNV assignment clone IDs', {
+    reference.clone.ids <- c('B', 'A', 'B', 'C');
+    invalid.clone.ids <- sapply(reference.clone.ids, function(x) paste0(x, 'B'));
+    
+    phylogeny <- SNV.counts <- data.frame(clone.ids = reference.clone.ids);
+    SNV.assignment <- data.frame(clone.ids = invalid.clone.ids);
+    
+    expect_error(
+        validate.clone.ids(phylogeny, SNV.assignment, SNV.counts), 'clone');
+    });
+
+test_that('validate.clone.ids handles invalid SNV count clone IDs', {
+    reference.clone.ids <- c('1', '3', 'B', '4');
+    invalid.clone.ids <- sapply(reference.clone.ids, function(x) paste0(x, 'A'));
+    
+    phylogeny <- SNV.assignment <- data.frame(clone.ids = reference.clone.ids);
+    SNV.counts <- data.frame(clone.ids = invalid.clone.ids);
+    
+    expect_error(
+        validate.clone.ids(phylogeny, SNV.assignment, SNV.counts), 'clone');
+    });
--- tests/testthat/test-utility.R
+++ tests/testthat/test-utility.R
@@ -32,3 +32,22 @@ test_that(
 
         expect_equal(as.numeric(reindexed), expected.values);
     });
+
+test_that('column.contains.all handles valid case', {
+    reference <- c('1', '2');
+    expect_true(column.contains.all(reference, reference));
+    });
+
+test_that('column.contains.all errors on non-vector reference input', {
+    reference <- NULL;
+    checked <- 1:10;
+    
+    expect_error(column.contains.all(reference, checked), 'vector');
+    });
+
+test_that('column.contains.all errors on non-vector checked input', {
+    reference <- c('A', 'C', 'D');
+    other <- NULL;
+    
+    expect_error(column.contains.all(reference, other), 'vector');
+    });
