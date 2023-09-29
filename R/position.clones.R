eb2f3102bd44462e443e3d2fa827ac382ce381f5
Dan Knight <danknight@mednet.ucla.edu>
2023-09-28 19:10:23 -0700

    Fix bug in angle override utility function

--- R/position.clones.R
+++ R/position.clones.R
@@ -81,7 +81,7 @@ position.clones <- function(v, tree, wid) {
     }
 
 position.nodes.fixed <- function(v, tree, fixed.angle, len) {
-    calculate.angles <- function(tree, fixed.angle) {
+    calculate.angles <- function(v, tree, fixed.angle) {
         node.ids <- c(v$id[[1]]);
         
         while (length(node.ids) > 0) {
@@ -103,11 +103,12 @@ position.nodes.fixed <- function(v, tree, fixed.angle, len) {
 
             node.ids <- append(node.ids, child.ids);
             }
-        
+
+        tree <- override.angles(tree, v);
         return(tree);
         }
 
-    tree <- calculate.angles(tree, fixed.angle);
+    tree <- calculate.angles(v, tree, fixed.angle);
 
     for (i in seq_along(v$id)) {
         vi <- v[i, ];
@@ -135,7 +136,6 @@ position.nodes.fixed <- function(v, tree, fixed.angle, len) {
         v[i,]$x <- x0;
         }
 
-    tree <- override.angles(tree, v);
     v <- reposition.clones(tree, v);
 
     clone.env <-  new.env(parent = emptyenv());
@@ -217,7 +217,22 @@ position.clones.no.vaf <- function(v, wid, spread = TRUE) {
     }
 
 override.angles <- function(tree, v) {
-    tree$angle[!is.na(v$angle)] <- v$angle[!is.na(v$angle)];
+    angle.overrides <- as.list(v$angle);
+    names(angle.overrides) <- v$id;
+    angle.overrides <- angle.overrides[!is.na(angle.overrides)];
+
+    angles <- apply(
+        tree,
+        MARGIN = 1,
+        FUN = function(x) {
+            node.id <- as.character(x['tip']);
+            angle.override <- angle.overrides[[node.id]];
+            angle <- if (is.null(angle.override)) x['angle'] else angle.override;
+            return(angle);        
+            }
+        );
+
+    tree$angle <- angles;
     return(tree);
     }
 
