42af8ac6b86af3586c2f6252453ce104964d51a4
Dan Knight <danknight@mednet.ucla.edu>
2024-02-06 10:03:54 -0800

    Unit tests for point data utilities

--- tests/testthat/test-utility.R
+++ tests/testthat/test-utility.R
@@ -32,3 +32,47 @@ test_that(
 
         expect_equal(as.numeric(reindexed), expected.values);
     });
+
+test_that(
+    'validate.data.frame.columns handles present columns', {
+        expected.columns <- c('x', 'y');
+
+        input.data <- data.frame(a = 1:10, b = 1:10);
+        colnames(input.data) <- expected.columns;
+
+        expect_no_error({ 
+            validate.data.frame.columns(
+                input.data,
+                expected.columns = expected.columns
+                );
+            });
+    });
+
+test_that(
+    'validate.data.frame.columns errors on missing columns', {
+        expected.columns <- c('x', 'y');
+
+        input.data <- data.frame(a = 1:10, b = 1:10);
+
+        expect_error(
+            object = { 
+                validate.data.frame.columns(
+                    input.data,
+                    expected.columns = expected.columns
+                    );
+                },
+            regexp = paste(expected.columns, collapse = ', ')
+            );
+    });
+
+test_that(
+    'get.encoded.distance returns valid relative values', {
+        # Testing the order, as the absolute values are not as important.
+        point.values <- c(-3, 1, 0, 2, -1);
+        points <- data.frame(x = point.values, y = point.values);
+
+        result <- get.encoded.distance(points);
+        expected.order <- order(point.values);
+
+        expect_equal(order(result), expected.order);
+    });
