ef739b7a7e078c660f5253eb56af0206d6323293
Dan Knight <danknight@mednet.ucla.edu>
2024-02-06 10:03:40 -0800

    Utilities for handling points along a line

--- R/utility.R
+++ R/utility.R
@@ -52,6 +52,46 @@ data.frame.to.array <- function(
     return(arr);
     }
 
+validate.data.frame.columns <- function(x, expected.columns) {
+    missing.columns <- !(expected.columns %in% colnames(x));
+
+    if (any(missing.columns)) {
+        stop(paste(
+            'Columns missing from input data:',
+            paste(expected.columns[which(missing.columns)], collapse = ', ')
+            ));
+        }
+    }
+
 degrees.to.radians <- function(degrees) {
     return(degrees * pi / 180);
+    }
+
+get.encoded.distance <- function(points) {
+    if (!is.data.frame(points)) { 
+        stop(paste(
+            "Input data \"points\" must be a data.frame",
+            paste0("(received \"", class(points), "\").")
+            ));
+        }
+    validate.data.frame.columns(points, expected.columns = c("x", "y"));
+
+    # Line calculations will not be valid with a single point
+    if (nrow(points) < 2) {
+        points$encoded.distance <- 0;
+        return(points);
+        }
+
+    coords <- as.matrix(points);
+    line.direction <- matrix(c(1, 1));
+
+    reference.point <- coords[1, ];
+
+    point.vectors <- coords - reference.point;
+    dot.products <- point.vectors %*% line.direction;
+    projections <- dot.products %*% t(line.direction);
+    distances.along.line <- sqrt(rowSums((point.vectors - projections)^2));
+    encoded.distances <- ifelse(dot.products >= 0, distances.along.line, -distances.along.line);
+
+    return(encoded.distances);
     }
