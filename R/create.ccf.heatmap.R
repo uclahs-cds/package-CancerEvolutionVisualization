2f46f9619cc18a1a2c6817e885e500acd85a349a
whelena <hwinata@mednet.ucla.edu>
2024-05-14 18:09:49 -0700

    set defaults for create.ccf.heatmap

--- R/create.ccf.heatmap.R
+++ R/create.ccf.heatmap.R
@@ -1,47 +1,35 @@
 create.ccf.heatmap <- function(
     hm.array,
     ccf.thres = NULL,
-    cls.dim = 'both',
-    cls.method = 'complete',
-    dist.method = 'euclidean',
-    hm.cols = c('white', 'blue'),
-    xaxis.lab = NULL,
+    cluster.dimensions = 'both',
+    clustering.method = 'complete',
+    distance.method = 'euclidean',
+    xaxis.lab = '',
     xlab.label = 'Mutations',
+    print.colour.key = FALSE,
+    colour.scheme = c('white', 'blue'),
     ...
     ) {
 
     if (!is.null(ccf.thres)) {
         hm.array[hm.array <= ccf.thres] <- 0;
         }
-    col.labels <- seq(0, 1, .2);
 
-    hm <- BoutrosLab.plotting.general::create.heatmap(
-        filename = NULL,
+    col.labels <- seq(min(hm.array), max(hm.array), length.out = 5);
+
+    return(BoutrosLab.plotting.general::create.heatmap(
         x = hm.array,
         force.clustering = TRUE,
-        cluster.dimensions = cls.dim,
-        clustering.method = cls.method,
-        rows.distance.method = dist.method,
-        cols.distance.method = dist.method,
+        cluster.dimensions = cluster.dimensions,
+        clustering.method = clustering.method,
+        rows.distance.method = distance.method,
+        cols.distance.method = distance.method,
         xaxis.lab = xaxis.lab,
         xlab.label = xlab.label,
-        # xlab.cex = 1,
-        # xaxis.cex = 0.6,
-        # xaxis.fontface = 1,
-        # xaxis.rot = 90,
         yaxis.lab = colnames(hm.array),
-        # ylab.cex = 1,
-        # yaxis.cex = 0.6,
-        # yaxis.fontface = 1,
-        # colourkey.cex = 0.6,
-        colour.scheme = hm.cols,
-        # left.padding = 1,
-        # right.padding = 1,
-        # resolution = 3000,
-        # width = 9,
-        # height = 5,
+        print.colour.key = print.colour.key,
         colourkey.labels.at = col.labels,
+        colour.scheme = colour.scheme,
         ...
-        );
-    return(hm);
+        ));
     }
