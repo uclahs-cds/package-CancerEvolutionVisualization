afddc4b98216cf3be2be9b9ef188d9227926221c
whelena <hwinata@mednet.ucla.edu>
2024-05-21 12:55:29 -0700

    plot SNV count for density plot

--- R/create.clone.genome.distribution.densityplot.R
+++ R/create.clone.genome.distribution.densityplot.R
@@ -8,11 +8,11 @@ create.clone.genome.distribution.densityplot <- function(
 
     return(BoutrosLab.plotting.general::create.scatterplot(
         filename = save.plt,
-        formula = scaled.y ~ x,
+        formula = count ~ x,
         data = density.df,
         groups = density.df$clone.id,
         xlab.label = 'Chromosome',
-        ylab.label = 'Density',
+        ylab.label = 'Number of SNVs',
         xlimits = c(0, sum(chr.info$length)),
         xaxis.lab = chr.info$chr,
         xat = chr.info$xat,
@@ -26,10 +26,11 @@ create.clone.genome.distribution.densityplot <- function(
     }
 
 calculate.density.and.scale <- function(cluster.df, total.nsnv) {
-    density <- density(x = cluster.df$genome.pos, bw = 'nrd', adjust = 0.05);
+    density <- density(x = cluster.df$genome.pos, bw = 'nrd', adjust = 0.05, na.rm = TRUE);
     density.df <- as.data.frame(density[c('x','y')]);
     density.df$clone.id <- unique(cluster.df$clone.id);
-    density.df$scaled.y <- density.df$y * nrow(cluster.df) / total.nsnv;
+    # density.df$scaled.y <- density.df$y * nrow(cluster.df) / total.nsnv;
+    density.df$count <- nrow(cluster.df) / sum(density.df$y) * density.df$y;
 
     return(density.df)
     }
