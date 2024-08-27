360b024f0ca72685cf6e22972843303d60d4b8a6
whelena <hwinata@mednet.ucla.edu>
2024-08-27 14:05:40 -0700

    add covariate using heatmap covariate

--- R/create.ccf.summary.heatmap.R
+++ R/create.ccf.summary.heatmap.R
@@ -5,6 +5,7 @@ create.ccf.summary.heatmap <- function(
     clone.order = NULL,
     sample.order = NULL,
     hm.col.scheme = c('white', 'blue'),
+    clone.colours = NULL,
     subplot.xlab.cex = 1.2,
     subplot.xaxis.cex = 1,
     subplot.xaxis.fontface = 'bold',
@@ -37,6 +38,17 @@ create.ccf.summary.heatmap <- function(
         arr                 <- arr[clone.order, sample.order];
         clone.df$clone.id   <- factor(clone.df$clone.id, levels = clone.order);
         sample.df$ID        <- factor(sample.df$ID, levels = sample.order);
+        }
+
+    if (!is.null(clone.colours)) {
+        clone.covariate <- list(
+            rect = list(
+                fill = clone.colours[clone.df$clone.id],
+                lwd = 1.5
+                )
+            );
+    } else {
+        clone.covariate <- NULL;
         }
 
     clone.bar <- BoutrosLab.plotting.general::create.barplot(
@@ -81,17 +93,22 @@ create.ccf.summary.heatmap <- function(
         yaxis.cex = subplot.yaxis.cex,
         yaxis.fontface = subplot.yaxis.fontface,
         print.colour.key = FALSE,
-        colour.scheme = hm.col.scheme
+        colour.scheme = hm.col.scheme,
+        xaxis.covariates = clone.covariate,
+        xaxis.covariates.y = if (is.null(clone.covariate)) NULL else -0.02,
+        # axis.xlab.padding = if (is.null(clone.covariate)) NULL else 3,
+        xaxis.tck = if (is.null(clone.covariate)) NULL else 4
         );
 
     legend.ccf <- BoutrosLab.plotting.general::legend.grob(
         list(
             legend = list(
                 title = 'CCF',
-                labels = c(min(arr), max(arr)),
+                labels = c(min(arr), rep('', legend.size), max(arr)),
                 colours = c('white', 'blue'),
                 border = 'black',
-                continuous = TRUE
+                continuous = TRUE,
+                cex = legend.label.cex
                 )
             ),
         size = legend.size,
@@ -106,9 +123,11 @@ create.ccf.summary.heatmap <- function(
         plot.objects.heights = c(0.3, 1),
         plot.objects.widths = c(1, 0.2),
         layout.skip = c(FALSE, TRUE, FALSE, FALSE),
-        legend = list(right = list(
-            fun = legend.ccf
-        )),
+        legend = list(inside = list(
+            fun = legend.ccf,
+            x = 0.9,
+            y = 0.8
+            )),
         ...
         ));
     }
