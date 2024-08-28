82d349eb901e0d8f85d2ef8b3006ce9782d882a1
whelena <hwinata@mednet.ucla.edu>
2024-08-28 13:14:40 -0700

    edit dendrogram expected test result

--- tests/testthat/test-dendrogram.R
+++ tests/testthat/test-dendrogram.R
@@ -1,7 +1,7 @@
 test_that(
     'Dendrogram case values', {
         load('data/branching.dendrogram.plots.Rda');
-        load('data/branching.dendrogram.data.Rda')
+        load('data/branching.dendrogram.data.Rda');
 
         result.tree <- SRCGrob(branching.dendrogram.test.data$tree);
         expect_true(compare.trees(
