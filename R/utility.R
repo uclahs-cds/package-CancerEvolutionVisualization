403d65fb7c4769879a34e9440b10f770e797420f
whelena <hwinata@mednet.ucla.edu>
2024-11-08 12:44:37 -0800

    move around util function when they are specifically called by a function

--- R/utility.R
+++ R/utility.R
@@ -63,29 +63,6 @@ validate.data.frame.columns <- function(x, expected.columns) {
         }
     }
 
-# conver chr coordinates to genome position
-get.genome.pos <- function(
-    snv.df,
-    genome.build = 'GRCh37',
-    chr.order = c(1:22, 'X', 'Y')
-    ) {
-
-    if (!(genome.build %in% c('GRCh37', 'GRCh38'))) {
-        stop('genome.build must be either GRCh37 or GRCh38')
-        }
-    assign('chr.info', get(genome.build));
-
-    snv.df$chr      <- droplevels(factor(snv.df$chr, levels = chr.order));
-    chr.info        <- chr.info[chr.info$chr %in% levels(snv.df$chr), c('chr', 'length')];
-    chr.info$chr    <- droplevels(factor(chr.info$chr, levels = chr.order));
-    chr.info$length <- as.numeric(chr.info$length);
-    chr.info$start  <- c(0, cumsum(chr.info$length[-length(chr.info$length)]));
-
-    snv.df$genome.pos <- chr.info[match(snv.df$chr, chr.info$chr), 'start'] + as.integer(snv.df$pos);
-
-    return(list('snv' = snv.df, 'chr.info' = chr.info));
-    }
-
 degrees.to.radians <- function(degrees) {
     return(degrees * pi / 180);
     }
@@ -117,49 +94,6 @@ get.encoded.distance <- function(points) {
     encoded.distances <- ifelse(dot.products >= 0, distances.along.line, -distances.along.line);
 
     return(encoded.distances);
-    }
-
-check.file.extension <- function(
-    filename,
-    supported.ext,
-    default.ext
-    ) {
-
-    ext <- tools::file_ext(filename);
-    if (!(ext %in% supported.ext)) {
-        message(paste(
-            'File extension', ext, 'is not in the list of supported extensions. Switching to default extension', default.ext
-            ));
-        filename <- gsub(paste0('.', ext), paste0('.', default.ext), filename);
-        ext <- default.ext;
-        }
-    return(list(name = filename, ext = ext));
-    }
-
-save.plot <- function(
-    grob,
-    filename,
-    units,
-    res,
-    bg,
-    ...
-    ) {
-
-    supported.extensions <- c('png', 'pdf', 'tiff', 'svg');
-    f <- check.file.extension(filename, supported.extensions, 'tiff');
-    if (f$ext == 'png') {
-        png(file = f$name, units = units, res = res, bg = bg, ...);
-    } else if (f$ext == 'tiff') {
-        tiff(file = f$name, units = units, res = res, bg = bg, ...);
-    } else if (f$ext == 'pdf') {
-        pdf(file = f$name, ...);
-    } else if (f$ext == 'svg') {
-        svg(file = f$name, bg = bg, ...);
-        }
-
-    grid.newpage();
-    grid.draw(grob);
-    dev.off();
     }
 
 update.descendant.property <- function(
@@ -196,4 +130,4 @@ update.descendant.property <- function(
         )
     }
     return(x)
-}
+    }
