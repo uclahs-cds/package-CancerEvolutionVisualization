7324939e518b369804f4890a7295742aa8d5bc4d
Dan Knight <danknight@mednet.ucla.edu>
2025-04-09 15:16:08 -0700

    Branching plotting direction regression test cases

--- tests/testthat/test-branching.R
+++ tests/testthat/test-branching.R
@@ -23,3 +23,36 @@ test_that(
             ));
         }
     );
+
+
+test_that(
+    'Radial branching "right" plotting direction case values', {
+        load('data/branching.radial.plots.Rda');
+        load('data/branching.radial.data.Rda')
+
+        result.tree <- SRCGrob(
+            branching.radial.test.data$tree,
+            plotting.direction = 'right'
+            );
+        expect_true(compare.trees(
+            result.tree,
+            branching.radial.right.example
+            ));
+        }
+    );
+
+test_that(
+    'Radial branching 30-degree plotting direction case values', {
+        load('data/branching.radial.plots.Rda');
+        load('data/branching.radial.data.Rda')
+
+        result.tree <- SRCGrob(
+            branching.radial.test.data$tree,
+            plotting.direction = 30
+            );
+        expect_true(compare.trees(
+            result.tree,
+            branching.radial.30deg.example
+            ));
+        }
+    );
