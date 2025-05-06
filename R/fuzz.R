e803f08f3d6f2be183ccf4cb08045f51a034cbce
Dan Knight <danknight@mednet.ucla.edu>
2025-05-06 14:02:32 -0700

    Randomize node color

--- R/fuzz.R
+++ R/fuzz.R
@@ -1,6 +1,7 @@
 randomize.tree <- function(
     tree.df,
     randomize.angles = TRUE,
+    randomize.node.color = TRUE,
     randomize.plotting.direction = TRUE,
     plotting.direction = NULL,
     ...
@@ -32,6 +33,29 @@ randomize.tree <- function(
             prep.plotting.direction(plotting.direction, radians = FALSE)
             );
         plotting.direction <- plotting.direction + rnorm(sd = angle.randomization.sd, n = 1);
+        }
+
+    if (randomize.node.color) {
+        node.color.randomization.prob <- 0.5;
+        node.color.scheme <- if (runif(1) <= node.color.randomization.prob) {
+            generate.random.color();
+        } else {
+            NA;
+            }
+
+        if (!('node.col' %in% colnames(tree.df))) {
+            tree.df$node.col <- node.color.scheme;
+        } else {
+            tree.df[is.na(tree.df$node.col), 'node.col'] <- node.color.scheme;
+            }
+        override.node.col.i <- sapply(
+            1:nrow(tree.df),
+            function(i) runif(1) <= node.color.randomization.prob
+            );
+        tree.df[override.node.col.i, 'node.col'] <- sapply(
+            1:sum(override.node.col.i),
+            function(i) generate.random.color()
+            );
         }
 
     result <- SRCGrob(
