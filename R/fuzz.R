f2ddc991f3c6a3c4712c93bac1aef0def6fe5d68
Dan Knight <danknight@mednet.ucla.edu>
2025-05-06 11:50:03 -0700

    Randomize tree spread in fuzz testing

--- R/fuzz.R
+++ R/fuzz.R
@@ -8,8 +8,21 @@ randomize.tree <- function(
     node.ids <- c(get.root.node(tree.df));
 
     spread.randomization.sd <- 0.5;
-    angle.randomization.sd <- 30;
+    if (randomize.angles) {
+        default.spread <- 1;
+        if (is.null(tree.df$spread)) {
+            tree.df$spread <- default.spread;
+        } else {
+            tree.df[is.na(tree.df$spread), 'spread'] <- 1;
+            }
+        tree.df$spread <- tree.df$spread + rnorm(
+            mean = 0,
+            sd = spread.randomization.sd, 
+            n = nrow(tree.df)
+            );
+        }
 
+    angle.randomization.sd <- 30;
     if (randomize.plotting.direction) {
         if (is.null(plotting.direction)) {
             plotting.direction <- sample(c('down', 'right', 'left', 'up'), size = 1);
